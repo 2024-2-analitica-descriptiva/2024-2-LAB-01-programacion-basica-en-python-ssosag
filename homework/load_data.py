381c16ef5a2bb678d355ead32c9dd0a9082e63f6
Santiago Sosa <ssosag@unal.edu.co>
2024-11-21 23:50:24 -0500

    coreccion path csv

--- homework/load_data.py
+++ homework/load_data.py
@@ -1,6 +1,6 @@
 def loadData():
     data = []
-    with open("files\input\data.csv", "r") as archivo:
+    with open("/files/input/data.csv", "r") as archivo:
         lineas = archivo.read().splitlines()
 
     for linea in lineas:
